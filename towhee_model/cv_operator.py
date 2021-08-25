06ac7bb93dc884b7b6333ecee462019c2290ebe1
derekdqc <578093244@qq.com>
2021-08-25 10:51:55 +0800

    cv operator design

---
+++ towhee_model/cv_operator.py
@@ -0,0 +1,63 @@
+# Copyright 2021 Zilliz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from abc import abstractclassmethod, ABC
+
+
+class Operator:
+    """
+    The base operator class.
+    """
+
+    def __init__(self):
+        self._params = {}
+        self.name = None
+
+    @property
+    def params(self):
+        return self._params
+
+    @params.setter
+    def params(self, dict):
+        # todo update parameter dict
+        raise NotImplementedError
+
+    @abstractclassmethod
+    def op_class(cls):
+        """
+        Get the interface Operator class
+        """
+        raise NotImplementedError
+
+
+class PytorchOperator(Operator, ABC):
+    """
+    The pytorch operator class
+    """
+    def __init__(self):
+        super().__init__()
+        self.trainer = None
+        # forward
+        self.model = None
+
+
+class ResNet50Img2Vec(PytorchOperator, ABC):
+    """
+    The underlying component of resnet class
+    """
+    def __init__(self):
+        super().__init__()
+        self.BasicBlock = None
+        self.Bottleneck = None
